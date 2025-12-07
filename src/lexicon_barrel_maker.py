7ecdd083647fd8022a6ddddebe3a489bb041aa2b
MSafiullah02 <muhammadsafiullahi2@gmail.com>
2025-12-08 00:45:58 +0500

    Added script to make barrels for lexicon

--- src/lexicon_barrel_maker.py
+++ src/lexicon_barrel_maker.py
@@ -3,7 +3,7 @@ os.makedirs("../lexicon", exist_ok=True) # ensures that the output directories e
 with open("lexicon.txt", "r", encoding="utf-8") as f:
     total = sum(1 for _ in f) # counts number of lines
 with open("lexicon.txt", "r") as infile:
-    outs = [open(f"{"../lexicon"}/lexicon{i + 1}.txt", "w") for i in range(27)] # opens all barrels for lexicon
+    outs = [open(f"../lexicon/lexicon{i + 1}.txt", "w") for i in range(100)] # opens all barrels for lexicon
     last_percent = -1
     i = 1
     for line in infile:
@@ -12,11 +12,7 @@ with open("lexicon.txt", "r") as infile:
         if percent != last_percent:
             print(f"{percent}% done", end='\r')  # shows progress in integer percentages so that showing progress doesn't take make the program inefficient
             last_percent = percent
-        ch = line.strip('\t')[0][0].lower()
-        if 'a' <= ch <= 'z': # if the first character is a letter, write it in the corresponding barrel
-            outs[ord(ch) - ord('a')].write(line)
-        else: # otherwise, write it in the last (extra) barrel
-            outs[26].write(line)
+        outs[int(line.strip().split('\t')[1]) % 100].write(line)
     for f in outs: # close all files
         f.close()
 print("\nDone!")
